8d059e1691a5228b18f52185ed6d8d24f6d5cdc2
github0ne <github0ne@gmail.com>
2015-09-09 14:25:29 -0400

    Added scenario to practice

---
+++ Branching-Workflow-Model/Christmas.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main() {
+    printf("Merry Christmas");
+}
+
+/**
+ * The goal this time is to have it so that we use some sort of branching.
+ *
+ * Jim will take a branch. Then he will modify the program on his branch. He will do
+ *      printf("The team here at PASA wishes you a ");
+ * Karen will also take a branch, but she will write:
+ *      printf("and Happy New Y3ar, DAWG.......\n");
+ * Karen will not realize that Jim has added the first part in.
+ * Jim will make a pull request, and the moderator will accept.
+ * 
+ * Then Karen will make a pull request. This will be denied because she spelled Year incorrectly
+ * and has unnecessarily included multiple periods at the end of her statement. Why? The world will never know.
+ * 
+ * Karen will make the requisite changes. She will make another pull request and then after this second approval,
+ * she will merge it with the existing document.
+ * 
+ * 
+ *
+ * 
+ * 
+ **/
+
+
